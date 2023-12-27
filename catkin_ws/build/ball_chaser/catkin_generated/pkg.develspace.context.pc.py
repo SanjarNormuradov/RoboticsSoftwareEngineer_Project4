91f0dc87551c710b1f564e688bc20bf60f5e0bea
Sanjar Normuradov <snnormuradov@gmail.com>
2023-12-26 22:40:54 -0800

    UPD: stop tracking build and devel folders. ADD: .gitignore

--- catkin_ws/build/ball_chaser/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/devel/include;/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/src/ball_chaser/include".split(';') if "/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/devel/include;/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/src/ball_chaser/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;geometry_msgs;sensor_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "ball_chaser"
-PROJECT_SPACE_DIR = "/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/sanjar/UW/Udacity/RoboticsSoftwareEngineer/project2/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
